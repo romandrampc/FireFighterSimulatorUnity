fb9181c2b7fdf870804531e43c8378701df05a80
Supanut Joohong <51392680+romandrampc@users.noreply.github.com>
2020-05-17 22:33:38 +0700

    add script scene manager

---
+++ Assets/Scripts/ControllerManager/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    GameController gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameController.instanceGame;
+    }
+    
+    void LoadMainMenu()
+    {
+
+    }
+
+    void LoadTrainingScene()
+    {
+        
+    }
+
+    void LoadSurvivorScene()
+    {
+
+    }
+
+    void LoadFireExtinScene()
+    {
+
+    }
+}
